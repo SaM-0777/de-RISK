14c06e68f1c807dd4d966c8c1af353622de8e3ba
SaM-0777 <sam.2.3.4.5.ranjan@gmail.com>
2025-10-25 01:31:52 +0530

    fix: server.js not found

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 const nextConfig: NextConfig = {
   /* config options here */
   outputFileTracingRoot: path.join(__dirname, "/web"),
+  distDir: './web/.next',
   images: {
     remotePatterns: [
       {
